3a3864d6b14fcfd1b0757f4a1d1476944d7ad866
Janzen Faidiban <janzenfaidiban@gmail.com>
2026-08-16 22:49:09 +0900

    major update: themes and content

--- resources/views/pages/home.blade.php
+++
@@ -1,9 +0,0 @@
-@extends('layouts.app')
-
-@section('title', 'YADUPA | Yayasan Anak Dusun Papua')
-
-@section('content')
-    @include('sections.hero')
-    @include('sections.about')
-    @include('sections.blog')
-@endsection
